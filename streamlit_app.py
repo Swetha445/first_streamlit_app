7d6e29cc7d3981545f65fa7e0a5dccca2e2d0e57
Swetha445 <138807875+Swetha445@users.noreply.github.com>
2023-07-07 11:28:41 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,5 +3,5 @@ import streamlit
 streamlit.title('My parents new healthy diner')
 
 streamlit.header('Swetha')
-Streamlit.text('Fruits')
-Streamlit.text('Veggies')
+streamlit.text('Fruits')
+streamlit.text('Veggies')
