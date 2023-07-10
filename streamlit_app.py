7fb4ec9adc7bf50b890721e5d4d0cb738946fa76
Swetha445 <138807875+Swetha445@users.noreply.github.com>
2023-07-10 16:21:15 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -47,7 +47,7 @@ except URLError as e:
 streamlit.header("The fruit load list contains:")
 def get_fruit_load_list():
      with my_cnx.cursor() as my_cur:
-          my_cur.execute("select * from pc_rivery_db.public.fruit_load_list")
+          my_cur.execute("select * from fruit_load_list")
           return my_cur.fetchall()
 #Add button
 if streamlit.button('Get Fruit Load List'):
@@ -55,7 +55,7 @@ if streamlit.button('Get Fruit Load List'):
    my_data_rows = get_fruit_load_list()
 streamlit.dataframe(my_data_rows)
 
-add_my_fruit = streamlit.text_input('What fruit would you like to add?','jackfruit')
-streamlit.write('Thanks for adding ', add_my_fruit)
+# add_my_fruit = streamlit.text_input('What fruit would you like to add?','jackfruit')
+# streamlit.write('Thanks for adding ', add_my_fruit)
 
-my_cur.execute("insert into fruit_load_list values ('from streamlit')")
+# my_cur.execute("insert into fruit_load_list values ('from streamlit')")
